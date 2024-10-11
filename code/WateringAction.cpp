6067725034634cc42b26d5e433471d07511bc3e3
Siyoung Lee <siyounglee1204@gmail.com>
2024-10-11 15:28:00 +1030

    Made printable into a few more stuff

--- code/WateringAction.cpp
+++ code/WateringAction.cpp
@@ -12,3 +12,9 @@ void WateringAction::reverseAction() {
     treeToModify->removeWater(waterAbsorbed);
     playerToModify->addWater(waterAdded);
 }
+
+void WateringAction::printData(){
+    cout << "Watering Action object" << endl;
+    cout << "Water Added: " << waterAdded << endl;
+    cout << "Water absorbed: " << waterAbsorbed << endl;
+}
